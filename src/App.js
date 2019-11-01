386f523035ccf0c3d0e3242d4873a1e3f325a3f2
unknown <maujr10@hotmail.com>
2019-10-31 22:02:33 -0600

    FireBase Login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import firebase from 'firebase'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDNJIm761mk3Emc2-RbsUSLwgL-XNBAbi4',
+  authDomain: 'chat-firebase-59ed9.firebaseapp.com'
+})
 
 function App() {
+  const [ isSignedIn, setSignedIn ] = useState(false)
+  const uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(
+      user => {
+        setSignedIn(!!user)
+      }
+    )
+  }, [])
+
+  const { currentUser } = firebase.auth();
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isSignedIn
+      ? <>
+          <div>
+            Signed In!
+          </div>
+          <button
+            onClick={() => {
+            firebase.auth().signOut()
+          }}
+          >
+            Sign Out
+          </button>
+          <h1>Welcome {currentUser.displayName}</h1>
+          <img alt="pp" src={currentUser.photoURL} />
+        </>
+      : <StyledFirebaseAuth
+        uiConfig={uiConfig}
+        firebaseAuth={firebase.auth()}
+      />
+      }
     </div>
   );
 }
